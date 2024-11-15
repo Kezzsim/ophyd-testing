24bc3d9e8de9e5f2407e8ad7f4dd626b5d799d9f
kari Barry <kezzsim@gmail.com>
2024-11-15 15:11:51 -0500

    Added somethign to get it into ipython

--- startup/01_test.py
+++ startup/01_test.py
@@ -1,12 +1,12 @@
+from detectors import SimDetectorTIFF
+from motors import SimMotor01
+
 from ophyd import EpicsMotor, EpicsSignalRO
 from bluesky import RunEngine
 
 from bluesky.plan_stubs import mv
 
-
-RE = RunEngine({})
-
 simMotor01 = SimMotor01("XF:31ID1-ES", name="simMotor01")
-RE(mv(simMotor01.y, 0.1))
-
 sim_det = SimDetectorTIFF("XF:31ID1-ES{ADSIM:01}", name="sim_det")
+
+print("Loaded")
